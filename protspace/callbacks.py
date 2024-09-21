c5989aad396239de3e69143822cc68407e212e9b
tsenoner <tobias.senoner94@gmail.com>
2024-09-21 15:08:00 +0200

    Add settings, download, and upload JSON button

--- protspace/callbacks.py
+++ protspace/callbacks.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
@@ -5,6 +8,7 @@ from dash_bio.utils import ngl_parser
 
 from .data_processing import prepare_dataframe
 from .plotting import create_2d_plot, create_3d_plot, save_plot
+from .config import NAN_COLOR
 
 
 def setup_callbacks(app, protspace):
@@ -14,19 +18,190 @@ def setup_callbacks(app, protspace):
             Input("projection-dropdown", "value"),
             Input("feature-dropdown", "value"),
             Input("protein-search-dropdown", "value"),
+            Input("apply-style-button", "n_clicks"),
+        ],
+        [
+            State("feature-value-dropdown", "value"),
+            State("marker-color-picker", "value"),
+            State("marker-shape-dropdown", "value"),
         ],
     )
-    def update_graph(selected_projection, selected_feature, selected_proteins):
-        df = prepare_dataframe(protspace.reader, selected_projection, selected_feature)
-        feature_colors = protspace.get_feature_colors(selected_feature)
+    def update_graph(
+        selected_projection,
+        selected_feature,
+        selected_proteins,
+        n_clicks,
+        selected_value,
+        selected_color,
+        selected_shape,
+    ):
+        # Update the changed values
+        if n_clicks is not None and selected_value is not None:
+            if selected_color:
+                # print(selected_color)
+                if selected_color['hex'].startswith('rgba'):
+                    selected_color_str = selected_color['hex']
+                else:
+                    selected_color_str = "rgba({r}, {g}, {b}, {a})".format(**selected_color['rgb'])
+                protspace.reader.update_feature_color(selected_feature, selected_value, selected_color_str)
+            if selected_shape:
+                protspace.reader.update_marker_shape(selected_feature, selected_value, selected_shape)
+
+        df = prepare_dataframe(
+            protspace.reader, selected_projection, selected_feature
+        )
+        feature_colors = protspace.reader.get_feature_colors(selected_feature)
+        marker_shapes = protspace.reader.get_marker_shape(selected_feature)
         projection_info = protspace.reader.get_projection_info(selected_projection)
 
         if projection_info["dimensions"] == 2:
-            return create_2d_plot(df, selected_feature, selected_proteins, feature_colors)
+            fig = create_2d_plot(df, selected_feature, selected_proteins)
         else:
-            return create_3d_plot(df, selected_feature, selected_proteins, feature_colors)
+            fig = create_3d_plot(df, selected_feature, selected_proteins)
+
+        # Apply all stored styles
+        for value in df[selected_feature].unique():
+            color = feature_colors.get(value)
+            shape = marker_shapes.get(value)
+            if color or shape:
+                fig.update_traces(
+                    marker=dict(
+                        color=color if color else None,
+                        symbol=shape if shape else None,
+                    ),
+                    selector=dict(name=value)
+                )
+            if value == "<NaN>":
+                fig.update_traces(
+                    marker=dict(
+                        color=NAN_COLOR,
+                    ),
+                    selector=dict(name=value)
+                )
+
+        return fig
+
+    @app.callback(
+        Output("feature-dropdown", "options"),
+        Output("feature-dropdown", "value"),
+        Output("projection-dropdown", "options"),
+        Output("projection-dropdown", "value"),
+        Output("protein-search-dropdown", "options"),
+        Input("upload-json", "contents"),
+        State("upload-json", "filename"),
+        prevent_initial_call=True,
+    )
+    def update_data_from_upload(contents, filename):
+        if contents is None:
+            raise PreventUpdate
+
+        content_type, content_string = contents.split(",")
+        decoded = base64.b64decode(content_string)
+        try:
+            if "json" in filename:
+                data = json.loads(decoded.decode("utf-8"))
+                protspace.reader.data = data  # Update the data in the reader
+                protspace.update_data()  # Update the ProtSpace object
+
+                # Update dropdowns
+                feature_options = [{"label": feature, "value": feature} for feature in protspace.features]
+                projection_options = [{"label": proj, "value": proj} for proj in protspace.projections]
+                protein_options = [{"label": pid, "value": pid} for pid in protspace.protein_ids]
+
+                # Select the first feature and projection
+                first_feature = protspace.features[0] if protspace.features else None
+                first_projection = protspace.projections[0] if protspace.projections else None
+
+
+                return (
+                    feature_options,
+                    first_feature,
+                    projection_options,
+                    first_projection,
+                    protein_options,
+                )
+        except Exception as e:
+            print(f"Error loading JSON: {e}")
+            return [], None, [], None, []
+
+    @app.callback(
+        Output("download-json", "data"),
+        Input("download-json-button", "n_clicks"),
+        prevent_initial_call=True,
+    )
+    def download_json(n_clicks):
+        if n_clicks == 0:
+            raise PreventUpdate
+
+        return dict(
+            content=json.dumps(protspace.reader.data, indent=2),
+            filename="protspace_data.json",
+        )
+
+    @app.callback(
+        Output("marker-style-controller", "style"),
+        Input("settings-button", "n_clicks"),
+        State("marker-style-controller", "style"),
+    )
+    def toggle_marker_style_controller(n_clicks, current_style):
+        if n_clicks is None:
+            return {"display": "none"}
+        if current_style["display"] == "none":
+            return {
+                "display": "block",
+                "width": "300px",
+                "padding": "20px",
+                "backgroundColor": "#f0f0f0",
+                "borderRadius": "5px",
+            }
+        else:
+            return {"display": "none"}
+
+    @app.callback(
+        Output("feature-value-dropdown", "options"),
+        Input("feature-dropdown", "value"),
+    )
+    def update_feature_value_options(selected_feature):
+        if selected_feature is None:
+            return []
+        unique_values = protspace.reader.get_unique_feature_values(
+            selected_feature
+        )
+        return [{"label": str(val), "value": str(val)} for val in sorted(unique_values)]
+
+    @app.callback(
+        Output("marker-color-picker", "value"),
+        Input("feature-dropdown", "value"),
+        Input("feature-value-dropdown", "value"),
+    )
+    def update_marker_color_picker(selected_feature, selected_value):
+        if selected_feature is None or selected_value is None:
+            raise PreventUpdate
+
+        feature_colors = protspace.reader.get_feature_colors(selected_feature)
+        if selected_value in feature_colors:
+            return {"hex": feature_colors[selected_value]}
+        else:
+            # If no color is defined for the selected value, return a default color
+            return {"hex": "#000000"}  # Default to black
+
+    @app.callback(
+        Output("marker-shape-dropdown", "value"),
+        Input("feature-dropdown", "value"),
+        Input("feature-value-dropdown", "value"),
+    )
+    def update_marker_shape_dropdown(selected_feature, selected_value):
+        if selected_feature is None or selected_value is None:
+            raise PreventUpdate
+
+        marker_shapes = protspace.reader.get_marker_shape(selected_feature)
+        if selected_value in marker_shapes:
+            return marker_shapes[selected_value]
+        else:
+            return None  # No shape selected
 
     if protspace.pdb_dir:
+
         @app.callback(
             Output("ngl-molecule-viewer", "data"),
             Input("protein-search-dropdown", "value"),
@@ -62,7 +237,9 @@ def setup_callbacks(app, protspace):
         if n_clicks == 0:
             raise PreventUpdate
 
-        projection_info = protspace.reader.get_projection_info(selected_projection)
+        projection_info = protspace.reader.get_projection_info(
+            selected_projection
+        )
         is_3d = projection_info["dimensions"] == 3
 
         fig = go.Figure(figure)
--- protspace/layout.py
+++ protspace/layout.py
@@ -1,6 +1,20 @@
 from dash import dcc, html
 from dash_bio import NglMoleculeViewer
+import dash_daq as daq
+from dash_iconify import DashIconify
 
+MARKER_SHAPES = [
+    'circle', 'circle-open', 'square', 'square-open', 'diamond', 'diamond-open',
+    'cross', 'x', 'triangle-up', 'triangle-down', 'pentagon', 'hexagon', 'star',
+    'hexagram', 'diamond-tall', 'diamond-wide', 'hourglass', 'bowtie',
+    'circle-cross', 'circle-x', 'square-cross', 'square-x', 'diamond-cross',
+    'diamond-x', 'asterisk', 'hash', 'y-up', 'y-down', 'arrow-up', 'arrow-down'
+]
+
+MARKER_SHAPES = [
+    'circle', 'circle-open', 'cross', 'diamond',
+    'diamond-open', 'square', 'square-open', 'x'
+    ]
 
 def create_layout(app):
     common_layout = [
@@ -26,53 +40,84 @@ def create_layout(app):
                 value=[],
                 placeholder="Search for protein identifiers",
                 multi=True,
-                style={"width": "48vw", "display": "inline-block"},
+                style={"width": "40vw", "display": "inline-block"},
             ),
-        ], style={"display": "flex", "justifyContent": "space-between"}),
-    ]
-
-    if app.pdb_dir:
-        plot_layout = html.Div([
+            html.Button(
+                DashIconify(icon="material-symbols:download", width=24, height=24),
+                id="download-json-button",
+                style={"marginLeft": "10px"},
+                title="Download JSON",
+            ),
+            dcc.Download(id="download-json"),
+            dcc.Upload(
+                id='upload-json',
+                children=html.Button(
+                    DashIconify(icon="material-symbols:upload", width=24, height=24),
+                    style={"marginLeft": "10px"},
+                    title="Upload JSON",
+                ),
+                multiple=False
+            ),
+            html.Button(
+                DashIconify(icon="carbon:settings", width=24, height=24),
+                id="settings-button",
+                style={"marginLeft": "10px"},
+            ),
+        ], style={"display": "flex", "justifyContent": "space-between", "alignItems": "center"}),
+        html.Div([
             html.Div([
                 dcc.Graph(id="scatter-plot", style={"height": "100%"}, responsive=True)
             ], style={
                 "border": "2px solid #dddddd",
                 "height": "calc(100vh - 200px)",
-                "width": "48vw",
+                "width": "100%",
                 "display": "inline-block",
                 "marginBottom": "20px",
             }),
             html.Div([
-                NglMoleculeViewer(
-                    id="ngl-molecule-viewer",
-                    width="100%",
-                    height="calc(100vh - 200px)",
-                    molStyles={
-                        "representations": ["cartoon"],
-                        "chosenAtomsColor": "white",
-                        "chosenAtomsRadius": 0.5,
-                        "molSpacingXaxis": 50,
-                        "sideByside": True,
-                    },
-                ),
-            ], style={
-                "border": "2px solid #dddddd",
-                "height": "calc(100vh - 200px)",
-                "width": "48vw",
-                "display": "inline-block",
-                "marginBottom": "20px",
-            }),
-        ], style={"display": "flex", "justifyContent": "space-between"})
-    else:
-        plot_layout = html.Div([
-            dcc.Graph(id="scatter-plot", style={"height": "calc(100vh - 200px)"}, responsive=True)
+                html.H4("Marker Style Settings", style={"marginBottom": "10px"}),
+                html.Div([
+                    html.Label("Select Feature Value:"),
+                    dcc.Dropdown(id="feature-value-dropdown", style={"marginBottom": "10px"}),
+                ]),
+                html.Div([
+                    html.Label("Marker Color:"),
+                    daq.ColorPicker(id="marker-color-picker", style={"marginBottom": "10px"}),
+                ]),
+                html.Div([
+                    html.Label("Marker Shape:"),
+                    dcc.Dropdown(
+                        id="marker-shape-dropdown",
+                        options=[{"label": shape.replace('-', ' ').title(), "value": shape} for shape in MARKER_SHAPES],
+                        style={"marginBottom": "10px"},
+                    ),
+                ]),
+                html.Button("Apply", id="apply-style-button"),
+            ], id="marker-style-controller", style={"display": "none", "width": "300px", "padding": "20px", "backgroundColor": "#f0f0f0", "borderRadius": "5px"}),
+        ], style={"display": "flex", "justifyContent": "space-between"}),
+    ]
+
+    if app.pdb_dir:
+        common_layout.append(html.Div([
+            NglMoleculeViewer(
+                id="ngl-molecule-viewer",
+                width="100%",
+                height="calc(100vh - 200px)",
+                molStyles={
+                    "representations": ["cartoon"],
+                    "chosenAtomsColor": "white",
+                    "chosenAtomsRadius": 0.5,
+                    "molSpacingXaxis": 50,
+                    "sideByside": True,
+                },
+            ),
         ], style={
             "border": "2px solid #dddddd",
             "height": "calc(100vh - 200px)",
+            "width": "48vw",
+            "display": "inline-block",
             "marginBottom": "20px",
-        })
-
-    common_layout.append(plot_layout)
+        }))
 
     common_layout.append(html.Div([
         html.Button("Download", id="download-button", n_clicks=0),

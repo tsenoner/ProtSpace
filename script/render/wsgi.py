2a93aa277713ed51d767ba0049484d1a88359694
tsenoner <tobias.senoner94@gmail.com>
2024-09-22 17:58:43 +0200

    add __init__.py to script

--- script/render/wsgi.py
+++ script/render/wsgi.py
@@ -1,7 +1,8 @@
 from protspace.app import ProtSpace
 
 # Initialize ProtSpace with the homo sapiens example JSON file
-protspace = ProtSpace("data/h_sapiens/h_sapiens_rbgaColored.json")
+# protspace = ProtSpace("data/h_sapiens/h_sapiens_rbgaColored.json")
+protspace = ProtSpace("data/3FTx/3FTx_customized.json")
 
 # Create the Dash app
 app = protspace.create_app()

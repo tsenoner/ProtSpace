7447ff95e831a1b50c7b3cc256895e9e661e9de5
tsenoner <tobias.senoner94@gmail.com>
2024-10-07 21:08:48 +0200

    Correct deployment path name

--- protspace/wsgi.py
+++ protspace/wsgi.py
@@ -5,7 +5,7 @@ protspace = ProtSpace(
     # pdb_zip="data/3FTx/3FTx_pdb.zip",
     # default_json_file="data/3FTx/3FTx_customized.json"
     pdb_zip="data/Pla2g2/Pla2g2_pdb.zip",
-    default_json_file="data/Pla2g2/protspace_files/Pla2g2_no_transperancy.json"
+    default_json_file="data/Pla2g2/protspace_files/Pla2g2_noTransperancy.json"
 
 )
 
